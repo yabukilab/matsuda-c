c48e9171bbea69f5f5776afc41bcbc03b5619766
Yakumo22 <s2242127TB@s.chibakoudai.jp>
2024-07-12 16:52:34 +0900

    20240712

--- htdocs/okyaku.php
+++ htdocs/okyaku.php
@@ -124,6 +124,7 @@ a
             <select name="departure_station" id="departure_station" required>
                 <?php
                 $sql_stations = "SELECT station_id, station_name FROM stations";
+a
                 $result_stations = $db->query($sql_stations);
                 while ($row = $result_stations->fetch_assoc()) {
                     echo "<option value='{$row['station_id']}'>{$row['station_name']}</option>";
