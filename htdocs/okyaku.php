ea75fa9f969d025705a6c290674bc21439970444
Yakumo22 <s2242127TB@s.chibakoudai.jp>
2024-07-12 16:42:57 +0900

    20240712

--- htdocs/okyaku.php
+++ htdocs/okyaku.php
@@ -5,17 +5,7 @@ if (!isset($_SESSION['user_name'])) {
     exit;
 }
 
-$servername = "127.0.0.1";
-$username = "testuser";
-$password = "pass";
-$dbname = "pm_train";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("接続に失敗しました: " . $conn->connect_error);
-}
-
+require 'db.php';
 // 予約処理のためのコードを追加
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['reserve'])) {
     $seat_id = $_POST['seat_id'];
